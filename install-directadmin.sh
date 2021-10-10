7a0b73471aa21fc3e4a5e7e4e1e06686c6a676e8
minhvinhdao <32488337+minhvinhdao@users.noreply.github.com>
2021-10-11 02:24:41 +0700

    create

--- install-directadmin.sh
+++ install-directadmin.sh
@@ -14,11 +14,9 @@
 #
 ###############################################################################
 yum update -y;
-
-yum install -y wget tar gcc gcc-c++ flex bison make bind bind-libs bind-utils openssl openssl-devel perl quota libaio libcom_err-devel libcurl-devel gd zlib-devel zip unzip libcap-devel cronie bzip2 cyrus-sasl-devel perl-ExtUtils-Embed autoconf automake libtool which patch mailx bzip2-devel lsof glibc-headers kernel-devel expat-devel psmisc net-tools systemd-devel libdb-devel perl-DBI perl-Perl4-CoreLibs perl-libwww-perl xfsprogs rsyslog logrotate crontabs file kernel-headers ipset nano;
-
+yum install -y nano wget tar gcc gcc-c++ flex bison make bind bind-libs bind-utils openssl openssl-devel perl quota libaio libcom_err-devel libcurl-devel gd zlib-devel zip unzip libcap-devel cronie bzip2 cyrus-sasl-devel perl-ExtUtils-Embed autoconf automake libtool which patch mailx bzip2-devel lsof glibc-headers kernel-devel expat-devel psmisc net-tools systemd-devel libdb-devel perl-DBI perl-Perl4-CoreLibs perl-libwww-perl xfsprogs rsyslog logrotate crontabs file kernel-headers net-tools;
+mkdir -p /usr/local/directadmin/custombuild/;
 wget -P /usr/local/directadmin/custombuild/ https://raw.githubusercontent.com/minhvinhdao/Finaly-All-In-One-DirectAdmin/main/php_extensions.conf && chmod 644 /usr/local/directadmin/custombuild/options.conf;
-
 wget -P /usr/local/directadmin/custombuild/ https://raw.githubusercontent.com/minhvinhdao/Finaly-All-In-One-DirectAdmin/main/options.conf && chmod 644 /usr/local/directadmin/custombuild/php_extensions.conf;
 
 pear install PHP_Archive -y;
