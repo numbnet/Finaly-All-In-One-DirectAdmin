010160a35a9d77e4d3a7e3a1d8387208743db06a
minhvinhdao <32488337+minhvinhdao@users.noreply.github.com>
2021-10-11 02:30:38 +0700

    create

--- config-auto-website.sh
+++ config-auto-website.sh
@@ -30,7 +30,7 @@ wget -P /usr/local/directadmin/custombuild/custom/openlitespeed/conf/ https://ra
 # DirectADmin PHP.ini
 cd /usr/local/directadmin/conf/;
 rm -f php.ini;
-wget https://raw.githubusercontent.com/minhvinhdao/final-DA/main/php.ini;
+wget https://raw.githubusercontent.com/minhvinhdao/Finaly-All-In-One-DirectAdmin/main/php.ini;
 chmod 644 php.ini;
 # PHP56
 rm -f /usr/local/php56/lib/php.ini;
@@ -55,7 +55,7 @@ cd custombuild;
 # Add Domain
 ln -s /usr/share/zoneinfo/Asia/Ho_Chi_Minh /etc/localtime;
 date;
-wget https://raw.githubusercontent.com/minhvinhdao/final-DA/main/add-domain.txt -P /var/www/html/ && chmod 644 /var/www/html/add-domain.txt;
+wget https://raw.githubusercontent.com/minhvinhdao/Finaly-All-In-One-DirectAdmin/main/add-domain.txt -P /var/www/html/ && chmod 644 /var/www/html/add-domain.txt;
 # DirectAdmin Config
 /usr/local/directadmin/directadmin set notify_on_license_update 0;
 /usr/local/directadmin/directadmin set check_subdomain_owner 0;
