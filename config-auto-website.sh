6937cb1a8daf8b50cbefe3cb5c522c6d212a0e37
minhvinhdao <32488337+minhvinhdao@users.noreply.github.com>
2021-10-11 02:29:26 +0700

    create

--- config-auto-website.sh
+++ config-auto-website.sh
@@ -27,6 +27,11 @@ wget -P /root/ https://raw.githubusercontent.com/minhvinhdao/Finaly-All-In-One-D
 cd /usr/local/directadmin/custombuild/;
 mkdir -p custom/openlitespeed/conf;
 wget -P /usr/local/directadmin/custombuild/custom/openlitespeed/conf/ https://raw.githubusercontent.com/minhvinhdao/Finaly-All-In-One-DirectAdmin/main/httpd-expires.conf && chmod 644 /usr/local/directadmin/custombuild/custom/openlitespeed/conf/httpd-expires.conf;
+# DirectADmin PHP.ini
+cd /usr/local/directadmin/conf/;
+rm -f php.ini;
+wget https://raw.githubusercontent.com/minhvinhdao/final-DA/main/php.ini;
+chmod 644 php.ini;
 # PHP56
 rm -f /usr/local/php56/lib/php.ini;
 wget -P /usr/local/php56/lib/ https://raw.githubusercontent.com/minhvinhdao/Finaly-All-In-One-DirectAdmin/main/php56/php.ini && chmod 644 /usr/local/php56/lib/php.ini;
@@ -47,6 +52,10 @@ cd custombuild;
 ./build update;
 ./build phpmyadmin;
 ./build rewrite_confs;
+# Add Domain
+ln -s /usr/share/zoneinfo/Asia/Ho_Chi_Minh /etc/localtime;
+date;
+wget https://raw.githubusercontent.com/minhvinhdao/final-DA/main/add-domain.txt -P /var/www/html/ && chmod 644 /var/www/html/add-domain.txt;
 # DirectAdmin Config
 /usr/local/directadmin/directadmin set notify_on_license_update 0;
 /usr/local/directadmin/directadmin set check_subdomain_owner 0;
