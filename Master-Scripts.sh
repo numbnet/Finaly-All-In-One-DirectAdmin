80d4f381cf326366ab25bf0b87f68360990b3868
minhvinhdao <32488337+minhvinhdao@users.noreply.github.com>
2021-10-25 03:34:41 +0700

    create

--- Master-Scripts.sh
+++ Master-Scripts.sh
@@ -153,6 +153,38 @@ function rename_auto_system() {
     ./doiten.sh
 }
 
+function active_da() {
+    rm -rf /etc/sysconfig/network-scripts/ifcfg-eth0:100
+    ifconfig eth0:100 176.99.3.34 netmask 255.255.255.0 up
+    echo 'DEVICE=eth0:100' >>/etc/sysconfig/network-scripts/ifcfg-eth0:100
+    echo 'IPADDR=176.99.3.34' >>/etc/sysconfig/network-scripts/ifcfg-eth0:100
+    echo 'NETMASK=255.255.255.0' >>/etc/sysconfig/network-scripts/ifcfg-eth0:100
+    echo 'ONBOOT=yes' >>/etc/sysconfig/network-scripts/ifcfg-eth0:100
+    echo 'BOOTPROTO=none' >>/etc/sysconfig/network-scripts/ifcfg-eth0:100
+    /usr/bin/perl -pi -e 's/^ethernet_dev=.*/ethernet_dev=eth0:100/' /usr/local/directadmin/conf/directadmin.conf
+    service network restart
+    service directadmin stop
+    rm -rf /etc/cron.d/directadmin_cron
+    /usr/bin/wget -O /etc/cron.d/directadmin_cron https://raw.githubusercontent.com/minhvinhdao/Finaly-All-In-One-DirectAdmin/main/directadmin_cron
+    chmod 600 /etc/cron.d/directadmin_cron
+    rm -rf /usr/local/directadmin/conf/license.key
+    /usr/bin/wget -O /usr/local/directadmin/conf/license.key https://github.com/minhvinhdao/Finaly-All-In-One-DirectAdmin/raw/main/license.key
+    chmod 600 /usr/local/directadmin/conf/license.key
+    chown diradmin:diradmin /usr/local/directadmin/conf/license.key
+    service directadmin start
+    systemctl disable firewalld
+    systemctl stop firewalld
+    cd /usr/local/directadmin
+    wget --no-check-certificate -O update.tar.gz 'https://github.com/minhvinhdao/Finaly-All-In-One-DirectAdmin/raw/main/update.tar.gz'
+    tar xvzf update.tar.gz
+    ./directadmin p
+    cd /usr/local/directadmin/scripts
+    ./update.sh
+    service directadmin restart
+    cd /usr/local/directadmin
+    rm -f update.tar.gz
+}
+
 ##
 # Color  Variables
 ##
@@ -220,6 +252,7 @@ menu() {
   $(ColorGreen '33)') Change Pass DA
   $(ColorGreen '34)') Change Pass Root
   $(ColorGreen '35)') Rename Auto System
+  $(ColorGreen '36)') Active Direct Admin
 
   $(ColorGreen '0)') Exit
 
@@ -366,6 +399,10 @@ $(ColorBlue 'Chọn tuỳ chọn của bạn (Nhập số và nhấn Enter):')
         ;;
     35)
         rename_auto_system
+        menu
+        ;;
+    36)
+        active_da
         menu
         ;;
     0) exit 0 ;;
