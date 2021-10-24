daf6679c1e77619c410082307ac85f90dd9ed6bf
minhvinhdao <32488337+minhvinhdao@users.noreply.github.com>
2021-10-25 06:31:59 +0700

    create

--- install-only-no-preconfig.sh
+++ install-only-no-preconfig.sh
@@ -15,7 +15,6 @@
 ###############################################################################
 yum update -y;
 yum install -y nano wget tar gcc gcc-c++ flex bison make bind bind-libs bind-utils openssl openssl-devel perl quota libaio libcom_err-devel libcurl-devel gd zlib-devel zip unzip libcap-devel cronie bzip2 cyrus-sasl-devel perl-ExtUtils-Embed autoconf automake libtool which patch mailx bzip2-devel lsof glibc-headers kernel-devel expat-devel psmisc net-tools systemd-devel libdb-devel perl-DBI perl-Perl4-CoreLibs perl-libwww-perl xfsprogs rsyslog logrotate crontabs file kernel-headers net-tools;
-pear install PHP_Archive -y;
 OS=`uname`;
 
 if [ "$(id -u)" != "0" ]; then
